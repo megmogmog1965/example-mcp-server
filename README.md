ec248c09092017e7d2b160119cbf382b7da7b38e
Yusuke Kawatsu <meg.mog.mog1965@gmail.com>
2025-04-02 21:56:24 +0900

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # example-mcp-server
+
 Example code for Anthropic MCP server.
+
+https://modelcontextprotocol.io/quickstart/server
+
+## How to build
+
+```bash
+npm install
+npm run build
+```
+
+## Run on Claude for Desktop
+
+To use this server with Claude Desktop, you need to configure it in your Claude Desktop configuration file. This file is located at:
+
+```bash
+~/Library/Application Support/Claude/claude_desktop_config.json
+```
+
+Example configuration:
+
+```json
+{
+  "mcpServers": {
+    "time": {
+      "command": "/path/to/node",
+      "args": [
+        "/path/to/example-mcp-server/build/index.js"
+      ]
+    }
+  }
+}
+```
+
+This configuration registers an MCP server named "time" that runs the built JavaScript code from this repository using Node.js.
---
+++ build/index.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+// Create server instance
+const server = new McpServer({
+    name: "time",
+    version: "1.0.0",
+    capabilities: {
+        resources: {},
+        tools: {},
+    },
+});
+// Register "get_current_time" tools
+server.tool("get_current_time", "Get the current time", {}, async () => {
+    const currentTime = new Date().toISOString();
+    return {
+        content: [
+            {
+                type: "text",
+                text: currentTime,
+            },
+        ],
+    };
+});
+async function main() {
+    const transport = new StdioServerTransport();
+    await server.connect(transport);
+    console.error("Weather MCP Server running on stdio");
+}
+main().catch((error) => {
+    console.error("Fatal error in main():", error);
+    process.exit(1);
+});
