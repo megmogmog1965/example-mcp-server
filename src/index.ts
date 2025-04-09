84cd3385cd73e360daa9dc58f4ec6e2e1e71d69c
Yusuke Kawatsu <meg.mog.mog1965@gmail.com>
2025-04-09 16:51:21 +0900

    typo.

--- src/index.ts
+++ src/index.ts
@@ -36,7 +36,7 @@ server.tool(
 async function main() {
   const transport = new StdioServerTransport();
   await server.connect(transport);
-  console.error("Weather MCP Server running on stdio");
+  console.error("Time MCP Server running on stdio");
 }
 
 main().catch((error) => {
